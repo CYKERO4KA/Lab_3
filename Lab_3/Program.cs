3995255902ce1f3b20ae7373ab5198b0b5d15bc4
CYKERO4KA <cykero4ka88@gmail.com>
2023-03-07 11:39:10 +0200

    Refactored

--- Lab_3/Program.cs
+++ Lab_3/Program.cs
@@ -7,25 +7,86 @@ namespace Lab_3
         static async Task Main(string[] args)
         {
             Interval interval1 = new Interval(6, 10, "A");
-            Interval interval2 = new Interval(2, 14, "B");
+            //Interval interval2 = new Interval(2, 14, "B");
 
             interval1.ShowInterval();
             Console.WriteLine(new string('=', 40));
-            await Manipulator.Save(interval2);
             await Manipulator.Read(interval1);
-            Console.WriteLine(new string('=', 40));
-            Console.ReadKey();
+            int index;
+            double coefficient;
+
+            Manipulator manipulator = new Manipulator();
+            
+            
+            while (true)
+            {
+                Console.WriteLine("\nChoose action: \n1.Show intervals\n2.Count interval lenght\n3.Shift to left" +
+                                  "\n4.Shift to right\n5.Compress interval\n6.Expand interval\n7.Compare intervals" +
+                                  "\n8.Add intervals\n9.Subtract intervals\n10.Save\n");
+                index = Convert.ToInt32(Console.ReadLine());
+                switch (index)
+                {
+                    case 1:
+                        Manipulator.interval?.ShowInterval();
+                        break;
+                    case 2:
+                        Console.WriteLine("The lenght is: "+Manipulator.interval?.CountIntervalLenght());
+                        break;
+                    case 3:
+                        Console.Write("Write a coefficient: ");
+                        coefficient = Convert.ToDouble(Console.ReadLine());
+                        Manipulator.interval?.ShiftToLeft(coefficient);
+                        break;
+                    case 4:
+                        Console.Write("Write a coefficient: ");
+                        coefficient = Convert.ToDouble(Console.ReadLine());
+                        Manipulator.interval?.ShiftToRight(coefficient);
+                        break;
+                    case 5:
+                        Console.Write("Write a coefficient: ");
+                        coefficient = Convert.ToDouble(Console.ReadLine());
+                        if (coefficient <= 1 && coefficient >= 0)
+                            Manipulator.interval?.CompressInterval(coefficient);
+                        else
+                            Console.WriteLine("You can't compress with this coefficient!");
+                        break;
+                    case 6:
+                        Console.Write("Write a coefficient: ");
+                        coefficient = Convert.ToDouble(Console.ReadLine());
+                        Manipulator.interval?.ExpandInterval(coefficient);
+                        break;
+                    case 7:
+                        Manipulator.interval?.CompareIntervals(interval1);
+                        break;
+                    case 8:
+                        Manipulator.interval?.AddIntervals(interval1);
+                        break;
+                    case 9:
+                        Manipulator.interval?.SubtractIntervals(interval1);
+                        break;
+                    case 10:
+                        manipulator.Save(Manipulator.interval);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect index!");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+
+            }
         }
         
     }
 
     class Manipulator
     {
-        public static async Task Save(Interval interval2)
+        public static Interval? interval;
+        public void Save(Interval interval)
         {
             using (FileStream fs = new FileStream("save.json", FileMode.OpenOrCreate))
             {
-                await JsonSerializer.SerializeAsync<Interval>(fs, interval2);
+                JsonSerializer.Serialize<Interval>(fs, interval);
                 Console.WriteLine("Saved");
             }
         }
@@ -34,33 +95,21 @@ namespace Lab_3
         {
             using (FileStream fs = new FileStream("read.json", FileMode.OpenOrCreate))
             {
-                Interval? interval = await JsonSerializer.DeserializeAsync<Interval>(fs);
-                interval?.CompressInterval(0.5);
-                interval?.CompareIntervals(interval1);
+                interval = await JsonSerializer.DeserializeAsync<Interval>(fs);
             }
         }
     }
-
     class Interval
     {
         private double _leftBorder;
         private double _rightBorder;
         private readonly string _name;
         
-        public double LeftBorder
-        {
-            get => _leftBorder;
-        }
-        public double RightBorder
-        {
-            get => _rightBorder;
-        }
+        public double LeftBorder => _leftBorder;
+        public double RightBorder => _rightBorder;
 
-        public string Name
-        {
-            get => _name;
-        }
-        
+        public string Name => _name;
+
 
         public Interval(double leftBorder, double rightBorder, string name)
         {
